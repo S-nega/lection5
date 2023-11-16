0c9e9547d3ea2765cba6263ca5ed23c2b0f2de52
s_nega <snezhana.golovko@gmail.com>
2023-11-16 14:59:21 +0600

    edit files, everythink work; have to fix folders

--- routes/files.js
+++ routes/files.js
@@ -14,57 +14,28 @@ router.get('/', function(req, res, next) {
 
 //look all 
 router.get('/read', async(req, res) => {
-  // console.log('reads')
   actualData = null;
-  // readAndPrintFile(filePath);
   directoryPath = './public/docs';
   readDirectory(res, directoryPath);
-  // res.sendFile(path.join(__dirname, "./public/docs/test.txt"));
 });
 
 
 //read one 
-router.get('/read/:name', async(req, res) => {
+router.get('/read/:name', async(req, res) => {// to fix folder
   console.log('reads')
-  // readAndPrintFile(filePath);
   const {name} = req.params;
-  // const name = req.body.name;
-  // const dir = req.body.dir;
-  // console.log("dir : ", dir);
-  // const {dir} = req.body.dir;
-  // if (dir){
-  //   console.log('dir is')
-  //   filePath = './public/docs/' + dir + name;
-  // }
-  // else{
-  //   console.log('dir no')
   filePath = './public/docs/' + name;
-  // }
-  // if (name.indexOf('.')){
 
-    readFile(res, filePath);
-    console.log("actual data: " + actualData)
-    if (actualData == null){
-      sendData(res, '/files/read/' + name);
-    }
-    else{
-      res.status(200).render('readf', {data: actualData, name:  name})
-      actualData = null;
-      console.log('deleted actual data')
-    }
-
-    // if (actualData == null){
-    //   res.redirect('/file/read/' + name);
-
-    // }
-    
-    
-  // }
-  // else{
-    // directoryPath = './public/docs/' + name;
-    // readDirectory(res, directoryPath);
-  // }
-  // res.sendFile(path.join(__dirname, "./public/docs/test.txt"));
+  readFile(res, filePath);
+  console.log("actual data: " + actualData)
+  if (actualData == null){
+    sendData(res, '/files/read/' + name);
+  }
+  else{
+    res.status(200).render('readf', {data: actualData, name:  name})
+    actualData = null;
+    console.log('deleted actual data')
+  }
 });
 
 
@@ -76,18 +47,21 @@ router.get('/write', async(req, res) => {
 })
 
 //edit page
-router.get('/edit/:name', async(req, res) => {
-  actualData = null;
-  const {name} = req.params
+router.get('/edit/:name', async(req, res) => {// to fix folder 
   console.log('edit')
+  const {name} = req.params;
+  filePath = './public/docs/' + name;
 
+  readFile(res, filePath);
+  console.log("actual data: " + actualData)
   if (actualData == null){
-    sendData(res, '/files/edit/' + name)
+    sendData(res, '/files/edit/' + name);
   }
   else{
-    res.render('edit', {data: actualData, name:name});
+    res.status(200).render('edit', {data: actualData, name:  name})
   }
-})
+});
+
 
 //adding
 router.post('/add', async(req, res) => {
@@ -107,7 +81,6 @@ router.post('/add', async(req, res) => {
           }
         });
         
-        // console.error(`Ошибка при чтении файла ${filePath}: ${err.message}`);
       } else {
         console.log("such name is already added");
         res.redirect('/files/write');
@@ -123,31 +96,14 @@ router.post('/add', async(req, res) => {
 //edit
 router.post('/edit/:name', async(req, res) => {
   actualData = null;
-  filePath = './public/docs/' + req.body.name
-  try{
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
+  const name = req.body.name
+  filePath = './public/docs/' + name
 
-        fs.writeFile(filePath, req.body.text, (err) => {
-          if (err) {
-              console.error(`Ошибка при записи в файл ${filePath}: ${err.message}`);
-          } else {
-              console.log(`Данные успешно записаны в файл ${filePath}`);
-              res.redirect('/files/read');
-          }
-        });
-        
-        // console.error(`Ошибка при чтении файла ${filePath}: ${err.message}`);
-      } else {
-        console.log("such name is already added");
-        res.redirect('/files/write');
-    }
-    })
-  }catch(error){
-    console.log(error)
-    res.status(500).json({message: error.message})
-  }
-  
+  const newData = req.body.data;
+
+  updateFileSync(filePath, newData);
+  res.redirect('/files/edit/' + name);
+
 })
 
 //delete
@@ -188,7 +144,6 @@ function readDirectory(res, directoryPath){
         dir += directoryPath[i];
         console.log(directoryPath[i]);
       }
-      // const dir = directoryPath % filePath;
       console.log("dir = ", dir);
       res.status(200).render('read', {data: data, dir: dir})
     }
@@ -200,12 +155,10 @@ function readFile(res, filePath){
     if (err) {
       directoryPath = './public/docs/' + name;
       readDirectory(res, directoryPath);
-      // console.error(`Ошибка при чтении файла ${filePath}: ${err.message}`);
     } else {
       actualData = data; 
       console.log( "read data: " + data);
       console.log('act in read data: ' + actualData)
-      // res.status(200).render('readf', {data: data, name:  name})
     } 
   });
 }
@@ -235,8 +188,17 @@ function deleteDirectory(directoryPath) {
 function sendData(res, address){
   res.redirect(address);
 }
-// function readAndPrintFile(filePath) {
-  
-// }
+
+
+function updateFileSync(filePath, newData) {
+  try {
+    fs.writeFileSync(filePath, newData, 'utf8');
+    console.log(`Данные успешно обновлены в файле ${filePath}.`);
+
+  } catch (err) {
+      console.error(`Ошибка при записи в файл ${filePath}: ${err.message}`);
+  }
+}
+
 
 module.exports = router;
